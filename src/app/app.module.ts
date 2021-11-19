e0f6d2b949311474d570dba2f8f3d0583b137d2c
Ricardo Roa <ricardo2roa@gmail.com>
2021-11-19 12:30:47 -0500

    Creaccion de Componente Personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { PersonaComponent } from './persona/persona.component';
-import { FormularioComponent } from './formulario/formulario.component';
+import { PersonaComponent } from './personas/persona/persona.component';
+import { FormularioComponent } from './personas/formulario/formulario.component';
 import { PersonasService } from './personas.service';
 import { LoggingService } from './loggingService.service';
 import {AppRoutingModule} from "./app-routing.module";
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonaComponent,
-    FormularioComponent
+    FormularioComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule,
