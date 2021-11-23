638337062a7e2bb5cd89f9c2c4c094b64b76714d
Ricardo Roa <ricardo2roa@gmail.com>
2021-11-23 01:34:02 -0500

    Se crea dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyANCbncyMD_mzM3Fu04UaD2I5fmzDDu6VI",
+    authDomain: "listado-personas-a71f1.firebaseapp.com",
+    databaseURL: "https://listado-personas-a71f1-default-rtdb.firebaseio.com",
+    projectId: "listado-personas-a71f1",
+    storageBucket: "listado-personas-a71f1.appspot.com",
+    messagingSenderId: "806988358408",
+    appId: "1:806988358408:web:971195444c7801283c42bd",
+    measurementId: "G-67BMF9ZJ8Q"
+  }
 };
